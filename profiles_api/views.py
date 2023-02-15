0c398dc463c51af86d627fd6a893782db42f1054
omarraafat14 <omarraafat14@gmail.com>
2023-02-16 01:39:21 +0200

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,8 @@ from rest_framework import filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from .serializers import HelloSerializer, UserProfileSerializer
 from .models import UserProfile
@@ -134,3 +136,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_201_CREATED
             )
 
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
